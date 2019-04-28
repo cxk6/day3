8727e2bee8fc0aa7be6047f5e1ee677cc0630342
cxk6 <1393171041@qq.com>
2019-04-28 21:41:04 +0800

    w

---
+++ src/java1/Welcome.java
@@ -0,0 +1,8 @@
+package java1;
+
+public class Welcome {
+	public static void main(String[] args) {	
+		System.out.println("小菜狗66666");
+	}
+
+}
---
+++ src/java1/java2.java
@@ -0,0 +1,9 @@
+package java1;
+
+public class java2 {
+	public static void main(String[] args) {
+		int a = Integer.parseInt("1+3");
+		System.out.println(a);
+	}
+
+}
